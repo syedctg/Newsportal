037f20f0dd0d95e3652473502a298d5f30e0fb97
syedctg <39445543+syedctg@users.noreply.github.com>
2018-05-20 03:39:50 +0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsPortal.Startup))]
+namespace NewsPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
